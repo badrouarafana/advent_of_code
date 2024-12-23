757a4e3cb6d75732a7d265a2a68da99a6cff74ca
badrouarafana <badr.ouarafana@hotmail.com>
2024-12-23 13:43:20 +0100

    added rec

--- day_19/main.py
+++ day_19/main.py
@@ -19,17 +19,46 @@ def checkIfExists(target , listOfItems , lastValue):
     if lastValue == 1:
         return 1
     cpt=0
-    temp = target
     while cpt < len(listOfItems):
         item = listOfItems[cpt]
         if len(target) ==0:
-            print(temp , "is OK")
+            #print(temp , "is OK")
             return 1
         if target.startswith(item):
             target = target[len(item) :]
             cpt =-1
+            print(item, end= ',')
         cpt +=1
     return 0
+
+def is_substring_in_array(substring, array_of_strings):
+    for string in array_of_strings:
+        if substring in string:
+            return True
+    return False
+
+
+def checkIfExists_part_2(target , listOfItems , foundPatterns):
+    final = ''
+    restart = False
+    i=0
+    j=0
+    
+    while i < len(listOfItems):
+        if len (target) ==0 :
+            return final
+        while j <len(listOfItems):
+            item = listOfItems [j]
+            if target.startswith(item) :
+                checkValue = final + ','+item if final != '' else item
+                if is_substring_in_array(checkValue, foundPatterns) == False :
+                    target = target[len(item) :]
+                    final += item + ','
+                    j = -1
+            j+=1
+        i+=1
+        j=0
+    return ''
 
 
 def perm (s):
@@ -39,21 +68,40 @@ def perm (s):
     return s
 
 
+
+def countOccurences (target , listOfItems):
+    patterns = []
+    temp_target = listOfItems
+    temp_val = ''
+    for i in range (len(listOfItems)):
+
+        temp_val =checkIfExists_part_2(target , temp_target , patterns)
+        print(temp_val)
+        if temp_val not in patterns and temp_val != '':
+            patterns.append(temp_val)
+        temp_target = perm(temp_target)
+    
+    return len(patterns)
+
+
+
+
 if __name__ == "__main__":
+    # for key in hashmap:
+    #     x= len(result)
+    #     for i in range( 0 ,x):
+    #         result = perm(result)
+    #         returnVal = checkIfExists(key , result , hashmap[key] )
+    #         hashmap[key] = returnVal
+    #     result= perm(result)
+
+    # count = sum(1 for value in hashmap.values() if value == 1)
+
+    # # Print the count
+    # print(f"Number of entries with value 1: {count}")
+
+
     for key in hashmap:
-        x= len(result)
-        for i in range( 0 ,x):
-            result = perm(result)
-            returnVal = checkIfExists(key , result , hashmap[key] )
-            hashmap[key] = returnVal
-        result= perm(result)
-
-    count = sum(1 for value in hashmap.values() if value == 1)
-
-    # Print the count
-    print(f"Number of entries with value 1: {count}")
-
-
-  
-        
+        val = countOccurences (key , result)
+        print(val)
 
---
+++ day_19/rec.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import math
+
+
+with open('input.txt', 'r') as file:
+    content = file.read().strip()
+    
+    result = [item.strip() for item in content.split(',')]
+
+with open('liste.txt', 'r') as file:
+    lines = [line.strip() for line in file]
+
+hashmap = {line: 0 for line in lines}
+
+
+
+def function (input, arr, hashMap):
+    ret = False
+    if len(input) ==0:
+        hashMap[input] = True
+        return True
+    if input in hashMap :
+        return hashMap[input]
+    
+    for i in arr:
+        if input.startswith(i):
+            ret= function (input[len(i):], arr , hashMap)
+    hashMap[input] = ret
+    return ret
+
+    
+
+if __name__ == "__main__":
+    # for key in hashmap:
+    #     x= len(result)
+    #     for i in range( 0 ,x):
+    #         result = perm(result)
+    #         returnVal = checkIfExists(key , result , hashmap[key] )
+    #         hashmap[key] = returnVal
+    #     result= perm(result)
+
+    # count = sum(1 for value in hashmap.values() if value == 1)
+
+    # # Print the count
+    # print(f"Number of entries with value 1: {count}")
+    tata = {}
+    cpt =0 
+    for key in hashmap:
+        
+        val = function (key , result, tata)
+        print(val)
+        if val :
+            cpt += 1
+        
+    
+    print(cpt )
+
